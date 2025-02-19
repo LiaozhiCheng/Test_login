d5062253d61b2f56dd111e2e60d6fc49957afab8
LiaozhiCheng <51042415+LiaozhiCheng@users.noreply.github.com>
2025-02-19 16:29:28 +0800

    change

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-
-
 from flask import Flask
-# from flask_mongoengine import MongoEngine
-from models import extension
-from views import (
-    login_api,
-    login_web,
-)
+
+from flask_security import Security
+
+from cram_school import models
+from cram_school.views import register_blueprint
+from cram_school.lib import config
+
 
 def create_app():
     app = Flask(__name__)
     app.jinja_env.auto_reload = True
-    app.config.from_object('app.config')  
-    
-    
-    
-    #register login method
-    extension.setup(app)
-    # extension.security.init_app(app,extension.user_datastore)
-    app.register_blueprint(login_api.login_api,url_prefix='')
-    app.register_blueprint(login_web.login_web,url_prefix='')       
+    app.config.from_object(config.Config())
+
+    # models setup
+    models.setup(app)
+
+    # security setup
+    Security(app, models.user.USER_DATASTORE)
+
+    # register app
+    register_blueprint(app)
+
     return app
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
     app.run()
--- views/login_api.py
+++ views/login_api.py
@@ -1,40 +1,23 @@
-from flask import flash, redirect, Blueprint, request, session, jsonify
-from flask_bcrypt import Bcrypt
-from models import extension
+from flask import request, Blueprint
+from flask_security import login_user
+from cram_school.models import user
 
-login_api = Blueprint('login_api', __name__)
-bcrypt = Bcrypt()
+login_api = Blueprint("login_api", __name__)
 
-#密碼加密
-def hashPassword(password):
-    pw_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-    return pw_hash
-
-#沒有權限導引畫面
-def unauthorized_callback():
-    flash("你沒有權限") 
-    return redirect('/login')
 
 # 設定未授權時轉跳畫面
 # security._state.unauthorized_handler(unauthorized_callback)
 
-@login_api.route('/login_user',methods = ['POST'])
-def login_user():
-    # try:
-    now_user = request.values.to_dict()
-    print(now_user)
-    #
-    now_user = extension.validate_user(now_user)
-    print("validate"+now_user)
-    if type(now_user) is not str:            
-    #    設置session
-        session['username'] = now_user['email']
-        session.permanent = True
-        
 
-        login_user(now_user)
-        return jsonify(now_user)
-    else:
-        return now_user
-    # except:
-    #     return "失敗"
+@login_api.route("/login_user", methods=["POST"])
+def validate():
+    email = request.values.get("email")
+    password = request.values.get("password")
+    cur_user = user.validate_user(email, password)
+    remember = True if request.values.get("rememberMe", "n") == "y" else False
+
+    if cur_user is None:
+        return "帳密錯誤"
+
+    login_user(cur_user, remember=remember)
+    return "success"
